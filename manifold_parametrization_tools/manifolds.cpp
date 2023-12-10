38d70e2fe32857aaa79c994c4f94f3e38aa03198
igrell <93985679+igrell@users.noreply.github.com>
2023-12-10 20:17:33 +0100

    Cleaning up

--- manifold_parametrization_tools/manifolds.cpp
+++ manifold_parametrization_tools/manifolds.cpp
@@ -9,14 +9,6 @@
 using namespace Eigen;
 using std::cout, std::cin, std::endl, std::vector, std::tuple;
 
-void parametrizeManifold(vector<vector<double>> &differentialMatrix, const unsigned n) {
-//    Matrix<double, n,n>
-}
-
-template<typename T>
-vector<T> fun(T x, T y, T z) {
-    return {(2 * x) + (x * x) + (y * z), (3 * y) + (x * y) - (z * z), (5 * z) + (y * y) + (2 * x * z)};
-}
 
 vector<vector<double>> totalDifferential(const vector<FunJet> &jets) {
     vector<vector<double>> res;
@@ -24,20 +16,28 @@ vector<vector<double>> totalDifferential(const vector<FunJet> &jets) {
     return res;
 }
 
-FunJet funJetWrapper(double x, unsigned& id, const unsigned N) { return {x, id, N}; }
-
-template<typename F, typename... Args>
-auto diffAtPoint(F f, Args... args) {
-    return f(args...);
-}
+/// @name funJetWrapper
+/// @brief Calls FunJest constructor with decrement of varFlag
+FunJet funJetWrapper(unsigned &varFlag, const unsigned &noOfVars, const double &x) { return {x, varFlag--, noOfVars}; }
 
 //template<typename F, typename... Args>
-//vector<vector<double>> D(F f, Args... args) {
+//auto applyFun(F f, Args... args) {
+//    return f(args...);
+//}
+
+// TODO
+//template<unsigned noOfVars, typename F, typename...Args>
+//vector<vector<double>> differentialAtPoint(F f, Args...args) {
+//    unsigned varFlag = noOfVars - 1;
 //    vector<vector<double>> res;
-//    return ();
+//    res.emplace_back(funJetWrapper(noOfVars,varFlag,args)...);
 //}
 
 // f(x,y) = ( (0 + x + y + x2 - 5xy - 2y2)/(1 + x - 4y), (1 + 2x - y + x2 - 4xy + 8y2)/(1 - 2x + 8y) )
+template<typename T>
+vector<T> fun(T x, T y, T z) {
+    return {(2 * x) + (x * x) + (y * z), (3 * y) + (x * y) - (z * z), (5 * z) + (y * y) + (2 * x * z)};
+}
 int main() {
     const unsigned N = 8;
     vector<double> p = {0., 0.5};
@@ -51,19 +51,17 @@ int main() {
     double y = 0;
     double z = 0;
     unsigned noOfVars = 3;
-//    auto jets = vector{FunJet(x, noOfVars), FunJet(y, noOfVars), FunJet(z, noOfVars)};
-//    FunJet::setVarOrder(jets,noOfVars);
-//    auto differential = totalDifferential(fun();
-    auto differential = totalDifferential(fun(FunJet(x, 0, noOfVars), FunJet(y, 1, noOfVars), FunJet(z, 2, noOfVars)));
+    unsigned varFlag =
+            noOfVars - 1; // as for some reason funJets are initialized from the last one, this flag goes from 2 to 0
+    auto differential = totalDifferential(fun(funJetWrapper(varFlag, noOfVars, x), funJetWrapper(varFlag, noOfVars, y),
+                                              funJetWrapper(
+                                                      varFlag, noOfVars, z)));
     for (auto &row: differential) {
         for (auto &el: row) {
             cout << el << " ";
         }
         cout << "\n";
     }
-
-//    auto res = diffAtPoint(fun<double>, 1, 2, 3);
-//    for (auto el: res) cout << el << " ";
 }
 
 #endif
