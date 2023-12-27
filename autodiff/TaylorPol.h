067d2a47837c8dcd0eaa2fb52ead733a84cdc2e9
igrell <93985679+igrell@users.noreply.github.com>
2023-12-27 21:55:31 +0100

    TaylorPol done for 1D

--- autodiff/TaylorPol.h
+++ autodiff/TaylorPol.h
@@ -1,6 +1,7 @@
 #ifndef INTERVAL_ANALYSIS_TAYLORPOL_H
 #define INTERVAL_ANALYSIS_TAYLORPOL_H
 
+#include <gmpxx.h>
 #include "vector"
 #include "sstream"
 #include "cassert"
@@ -12,46 +13,63 @@ class TaylorPol {
     vector<T> coeffs;
     size_t order;
 public:
-    explicit TaylorPol(const vector<T> &_coeffs) : coeffs(_coeffs), order(_coeffs.size()) {}
+    explicit TaylorPol(const vector<T> &_coeffs) : coeffs(_coeffs), order(_coeffs.size() - 1) {}
 
-    explicit TaylorPol(const size_t _order) : coeffs(vector<T>(_order)), order(_order) {}
+    explicit TaylorPol(const size_t _order) : coeffs(vector<T>(_order + 1)), order(_order) {}
 
-    vector<T>& getCoeffs() const { return coeffs; }
+    vector<T> getCoeffs() const { return coeffs; }
 
-    size_t getOrder() const { return order; }
+    [[nodiscard]] size_t getOrder() const { return order; }
+
+    vector<T> getDiffs();
 
     T &operator[](const unsigned id) {
-        assert(id > -1 and id <= order);
+        assert(id <= order);
         return coeffs[id];
     }
 
-    TaylorPol<T> operator+(const TaylorPol<T> &q) {
-        const TaylorPol<T> &p = *this;
-        assert(p.getOrder() == q.getOrder());
-        TaylorPol<T> r = p;
-        for (unsigned i = 0; i <= p.getOrder(); ++i) r[i] += q[i];
-        return r;
-    }
+    TaylorPol<T> operator+(const TaylorPol<T> &q);
 
     TaylorPol<T> operator-(const TaylorPol<T> &q) {
         const TaylorPol<T> &p = *this;
         assert(p.getOrder() == q.getOrder());
         TaylorPol<T> r = p;
-        for (unsigned i = 0; i <= p.getOrder(); ++i) r[i] -= q[i];
+        for (unsigned i = 0; i <= p.getOrder(); ++i) r[i] -= q.getCoeffs()[i];
         return r;
     }
 
     TaylorPol<T> operator*(const TaylorPol<T> &q) {
         const TaylorPol<T> &p = *this;
+        assert(p.getOrder() == q.getOrder());
+        TaylorPol<T> r(p.getOrder());
+        T tmpSum = 0;
+        for (unsigned i = 0 ; i <= p.getOrder() ; ++i)
+            for (unsigned j = 0 ; j <= i ; ++j) r[i] += p.getCoeffs()[j] * q.getCoeffs()[i-j];
+        return r;
     }
 };
 
+template<class T>
+vector<T> TaylorPol<T>::getDiffs() {
+    vector<T> _coeffs = getCoeffs();
+    std::transform(_coeffs.begin(),_coeffs.end(),_coeffs.begin(),[](T x){ return factorial(x); }); // TODO
+    return vector<T>();
+}
+
+template<class T>
+TaylorPol<T> TaylorPol<T>::operator+(const TaylorPol<T> &q) {
+    const TaylorPol<T> &p = *this;
+    assert(p.getOrder() == q.getOrder());
+    TaylorPol<T> r = p;
+    for (unsigned i = 0; i <= p.getOrder(); ++i) r[i] += q.getCoeffs()[i];
+    return r;
+}
 
 template<class T>
 ostream &operator<<(ostream &ostream, const TaylorPol<T> &p) {
     ostream << "[";
-    for (const auto &coeff: p.getCoeffs()) ostream << coeff << " ";
-    ostream << "]\n";
+    for (unsigned i = 0; i < p.getOrder() ; ++i) ostream << p.getCoeffs()[i] << " ";
+    ostream << p.getCoeffs()[p.getOrder()] << "]\n";
     return ostream;
 }
 
--- testing.cpp
+++ testing.cpp
@@ -7,7 +7,8 @@
 int main() {
     TaylorPol<double> p({1,1,1});
     TaylorPol<double> q({2,2,2});
-    std::cout << (p+q);
+    std::cout << factorial()
+    auto diffs = p.getDiffs();
 //    Henon henon(1.0,1.0,1);
 //    std::vector<Interval> vec{Interval(1,2), Interval(3,4)};
 //    std::cout << henon(vec)[0] << " " << henon(vec)[1];
