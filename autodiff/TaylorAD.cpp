c6ab58a22afaf42cac8d675042555d90bd7c4fa7
igrell <93985679+igrell@users.noreply.github.com>
2023-12-28 12:54:47 +0100

    Fixed addition

--- autodiff/TaylorAD.cpp
+++ autodiff/TaylorAD.cpp
@@ -1,5 +1,1 @@
-//
-// Created by igor on 12/28/23.
-//
-
 #include "TaylorAD.h"
--- autodiff/TaylorAD.h
+++ autodiff/TaylorAD.h
@@ -1,16 +1,60 @@
-//
-// Created by igor on 12/28/23.
-//
+#ifndef TAYLORAD_H
+#define TAYLORAD_H
 
-#ifndef INTERVAL_ANALYSIS_TAYLORAD_H
-#define INTERVAL_ANALYSIS_TAYLORAD_H
+#include "vector"
+#include "TaylorPol.h"
+#include "cassert"
+#include "sstream"
 
+using std::vector, std::ostream;
 
+/// @class TaylorJet
+/// @brief Implementing FAD of any order for function p : R -> R^n
+template<class T>
+class TaylorJet {
+    vector<TaylorPol<T>> pols; // p^[i] (t)
+    vector<vector<T>> diffs; // p^(i) (t)
 
-class TaylorAD {
+public:
 
+    explicit TaylorJet(const size_t order) {
+        pols = vector<TaylorPol<T>>(order);
+    }
+
+    explicit TaylorJet(const vector<TaylorPol<T>> _pols) : pols(_pols), diffs(vector<vector<T>>(_pols.size())) {}
+
+    [[nodiscard]] size_t getOrder() const;
+
+    TaylorPol<T> polAt(const unsigned int k) const { return pols.at(k); }
+
+    TaylorPol<T> &polAt(const unsigned int k) { return pols.at(k); };
+
+    vector<T> diffAt(const unsigned int k) const { return diffs.at(k); }
+
+    vector<T> &diffAt(const unsigned int k) { return diffs.at(k); }
+
+    TaylorJet<T> operator+(TaylorJet<T> &q) const;
 };
 
+template<class T>
+TaylorJet<T> TaylorJet<T>::operator+(TaylorJet<T> &q) const {
+    const TaylorJet<T> &p = *this;
+    assert(p.getOrder() == q.getOrder());
+    vector<TaylorPol<T>> newpols;
+    for (unsigned i = 0; i <= p.getOrder(); ++i) newpols.emplace_back(p.polAt(i) + q.polAt(i));
+    return TaylorJet<T>(newpols);
+}
 
+template<class T>
+size_t TaylorJet<T>::getOrder() const {
+    return pols.size() - 1;
+}
 
-#endif //INTERVAL_ANALYSIS_TAYLORAD_H
+template<class T>
+ostream &operator<<(ostream &ostream, const TaylorJet<T> &jet) {
+    ostream << "k |  p[k]\n";
+    for (unsigned i = 0; i <= jet.getOrder(); ++i) ostream << i << ": " << jet.polAt(i) << "\n";
+    return ostream;
+}
+
+#endif //TAYLORAD_H
