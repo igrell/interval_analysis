fa860de317c52e47b34b0e33a44588bac97cc7ac
igrell <93985679+igrell@users.noreply.github.com>
2025-03-30 19:26:12 +0200

    gitignore

--- testing.cpp
+++
@@ -1,9 +0,0 @@
-#include "Interval.h"
-#include "iostream"
-
-int main() {
-    Interval x{0.01,0.99};
-    Interval y{-0.33,0.27};
-    auto f = [](auto x, auto y){ return 3.8*x*(1-x) - 0.1*y;};
-    std::cout << f(x,y);
-}
